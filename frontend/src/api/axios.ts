2b1eb31839443f41e855bd39c20a04bb3fe7b673
JaspherXIII <157330223+JaspherXIII@users.noreply.github.com>
2026-02-12 13:47:17 +0800

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,12 +1,14 @@
+// src/api/axios.ts
 import axios from "axios";
 
-const apiBaseUrl = import.meta.env.VITE_API_URL;
+const apiBaseUrl = import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
   baseURL: apiBaseUrl ? `${apiBaseUrl}/api` : "/api",
   withCredentials: true,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
   },
 });
 
