29d08c300a300d900d3fa30666181d6014f49c48
Sherwin Adonis M. Vizcarra - II <adnis7222@gmail.com>
2026-02-14 14:30:26 +0800

    latest build 0.4a.5

--- frontend/dist/assets/download-DAEcyeSn.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-BOrxGUId.js";const a=[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]],e=o("arrow-up",a);const t=[["path",{d:"M12 15V3",key:"m9g1x1"}],["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["path",{d:"m7 10 5 5 5-5",key:"brsn70"}]],n=o("download",t);export{e as A,n as D};
